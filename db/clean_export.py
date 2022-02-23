70d5b7a8ba5e58cc6b8a3b04d4d7f5345f0a1342
SyamKumar19 <75555467+SyamKumar19@users.noreply.github.com>
2022-02-23 10:21:53 +0530

    Add files via upload

---
+++ db/clean_export.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import sqlite3
+
+df = pd.read_csv('covid_19_data.csv')
+
+df.columns = ['SNo', 'ObservationDate', 'State', 'Country',
+              'LastUpdate', 'Confirmed', 'Deaths', 'Recovered']
+
+for i in df.columns:
+    if df[str(i)].dtype == 'float64':
+        df.fillna(0.0, inplace=True)
+    else:
+        df.fillna("UnKnown", inplace=True)
+
+df['ObservationDate'] = pd.to_datetime(df['ObservationDate'])
+
+df['LastUpdate'] = pd.to_datetime(df['LastUpdate'])
+
+print(df.head(3))
+
+df.to_csv("covid_clean_data.csv", index=False)
+
+connection = sqlite3.connect('covid.db')
+
+df.to_sql('covid', connection, if_exists='replace', index=False)
+
+connection.close()
+
